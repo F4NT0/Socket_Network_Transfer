5cae17e84201db7235e887e89e4b736f108f1bd5
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-11-19 20:09:19 -0300

    #5 File Manager

---
+++ Project/fileManager.py
@@ -0,0 +1,42 @@
+#
+# FILE MANAGER
+# Instalação: sudo apt-get install python3-tk
+#
+
+from tkinter import *
+from tkinter import filedialog
+
+# Tela do File Manager
+def browseFile():
+    filename = filedialog.askopenfilename(initialdir = "/",title = "Selecione um Arquivo",filetypes=[("Tipo de Arquivo","*.txt")])
+    # Alterar o conteudo
+    label_file.configure(text = "Arquivo Aberto: " + filename)
+
+# Criando uma Tela Inicial
+window = Tk()
+
+# Definindo o título da tela
+window.title("Gerenciador de Arquivos")
+
+# Definindo tamanho da tela
+window.geometry("800x500")
+
+# Definindo a cor de fundo da tela
+window.config(background = "white")
+
+# Label interno da tela
+label_file = Label(window,text = "Abrindo Arquivo para envio",width = 100, height = 4,fg = "green")
+
+# Botão de encontrar o Arquivo
+button_explore = Button(window,text = "Encontrar Arquivo",command = browseFile)
+
+# Botão de fechar
+button_exit = Button(window,text = "Sair",command = exit)
+
+# Colocar as informações na tela
+label_file.grid(column = 1, row = 1)
+button_explore.grid(column = 1, row = 2)
+button_exit.grid(column = 1, row = 3)
+
+# Esperando uma resposta
+window.mainloop()
