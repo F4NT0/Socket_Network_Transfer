726b8f35f141cf02f35a7f1775598bb14f6d37b5
LuizGuerra <luizpedrofg@gmail.com>
2020-11-23 15:27:02 -0300

    naive slow start implementation

--- Project/auxiliar.py
+++ Project/auxiliar.py
@@ -1,4 +1,15 @@
 import time
 
-def hexa(value):
-    return hex(value[2:]).zfill(3).encode("utf-16")
+# Converte um número em seu hexadecimal
+def hexa(decimal):
+    return hex(decimal[2:]).zfill(3).encode("utf-16")
+
+def deca(hexadecimal):
+    return int("0x{}".format(hexadecimal), 16)
+
+# Formatação de header pro cabeçalho
+def formatUDP(haveNext, segmentation, message):
+    flag = int(haveNext).encode("utf-16")
+    index = str(segmentation).zfill(3).encode("utf-16")
+    return flag + index + message
+
--- Project/udpclient.py
+++ Project/udpclient.py
@@ -8,26 +8,25 @@ import socket
 import time
 import random
 
+from Project.auxiliar import formatUDP
+
 # Variáveis
 file = open("book.txt", "r", encoding='utf-8', errors="strict")
-encodedStr = file.read(100).encode("utf-16", errors="replace")
+encodedStr = file.read(1020).encode("utf-16", errors="replace")
 serverAddressPort = ("127.0.0.1", 8184)
-bufferSize = 1024
+bufferSize = 1020
 packages = [encodedStr[i:i+bufferSize] for i in range (0, len(encodedStr), bufferSize)]
 
-# Variáveis do Slow Start
+# Variáveis para mandar pacotes
 cwnd = 1
 threshold = 64
 mtu = 1500
 mss = mtu - 40
 roundTripTime = -1
+index = 0
 
 # Criando Socket UDP do cliente
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-# Funções auxiliares de formatação do cabeçalho
-def formatUDP(segmentation, message):
-    return "{:04d}".format(int(segmentation)).encode("utf-16") + message
 
 # Verificando RTT
 initialTime = time.time()
@@ -40,22 +39,21 @@ roundTripTime = 0.001 if roundTripTime==0 else roundTripTime
 print("Round-Trip-Time: {}".format(roundTripTime))
 
 
+# Algoritmo de Slow Start
 def slowStart():
-    global cwnd
+    global cwnd, index
     while cwnd < threshold:
-        for index in range(cwnd):
-            UDPClientSocket.sendto()
-    initialTime = time.time()
-
+        for i in range(cwnd):
+            UDPClientSocket.sendto(
+                formatUDP(True, index, packages[index]),
+                serverAddressPort)
+            index += 1
+        cwnd += cwnd
     print("Reached threshold!" if (cwnd>=threshold) else "Ended streaming!")
 
 
 
 # Enviando mensagem ao Servidor
-for package in packages:
-    UDPClientSocket.sendto(package,serverAddressPort)
-    # Pegando mensagem do Servidor
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    print("Mensagem do Servidor: {}".format(msgFromServer[0]))
+
 
 file.close()
